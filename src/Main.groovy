70437b68b577be59bc61c5456666f5f5cf7bf225
Andrei Sharapov <adras.sad@gmail.com>
2023-08-11 03:25:35 +0300

    Integration ITSM with GanttPRO

--- src/Main.groovy
+++ src/Main.groovy
@@ -9,23 +9,40 @@ class Task {
   public def startDate;
   public def endDate;
   public String responsible;
-  public ArrayList comments;
-  public ArrayList subTasks;
+  public ArrayList<Comment> comments = new ArrayList<>();
+  public ArrayList<Task> subTasks = new ArrayList();
 }
+
+class User{
+  public int id;
+  public String firstName;
+  public String lastName;
+}
+
+class Comment{
+  public int id;
+  public User user;
+  public def createdAt;
+  public def comment;
+}
+
 static void main(String[] args) {
   def get = getConnection('https://api.ganttpro.com/v1.0/projects/')
   if (get.getResponseCode().equals(200)) {
     def list = getListData(get)
     for (i in 0..<list.size()) {
       if (list[i].name.contains("ITSM")){
-        def dataProject = getTasks(list[i].projectId)
+        def tasks = getTasks(list[i].projectId)
+        for (Task task: tasks){
+
+        }
         //модуль для создания заявки и подзадач
       }
     }
   }
 }
 
-public def getTasks(def idProject){
+def getTasks(def idProject){
   def dataUsers = getUsers()
   FillerTasks fillerTask = new FillerTasks();
   fillerTask.setDataResources(getResources())
@@ -56,24 +73,24 @@ class FillerTasks {
     if(firstID==0){
       firstID = dataTask.parent
     }
-    if(dataTask.parent=firstID){
-      def task = generateStrList(dataTask)
+    if(dataTask.parent==firstID){
+      def task = generateTask(dataTask)
       if(task!=null){
         tasks.put(dataTask.id, task)
       }
     }else{
       def task = tasks.get(dataTask.parent)
-      if(dataTask!=null){
-        def subTask = generateStrList(dataTask)
-        if(task!=null) {
-          dataTask.subTasks.add(subTask)
-          dataTask.put(dataTask.id, task)
+      if(task!=null){
+        def subTask = generateTask(dataTask)
+        if(subTask!=null) {
+          task.subTasks.add(subTask)
+          tasks.put(dataTask.parent, task)
         }
       }
     }
   }
 
-  def generateStrList(def dataTask){
+  def generateTask(def dataTask){
     boolean loadToITSM = getValueCastumFild(dataResources, dataTask, 'Выгружать в ITSM');
     if (loadToITSM==false){
       return
@@ -86,9 +103,30 @@ class FillerTasks {
     task.startDate = dataTask.startDate;
     task.endDate = dataTask.endDate;
     task.responsible = getValueCastumFild(dataResources, dataTask, 'Ответственный')
+    for (def dataComment:dataTask.get("comments")){
+      task.comments.add(getComment(dataComment))
+    }
     return task
 
   }
+
+  Comment getComment(def dataComment){
+    Comment comment = new Comment();
+    comment.id = dataComment.id;
+    comment.comment = dataComment.comment;
+    comment.createdAt = dataComment.createdAt;
+    comment.user = getUser(dataComment.get("user"))
+    return comment
+  }
+
+  User getUser(def dataUser){
+    User user = new User();
+    user.id = dataUser.resourceId;
+    user.firstName = dataUser.firstName;
+    user.lastName = dataUser.lastName;
+    return user
+  }
+
   def getValueCastumFild(def dataResources, def task, String field){
     def value;
     if (task.get('customFields')){
@@ -125,6 +163,7 @@ def getResources(){
   }
   return map
 }
+
 def getUsers(){
   Map<Integer, LazyMap> map = new HashMap<>()
   def get = getConnection('https://api.ganttpro.com/v1.0/users')
@@ -134,24 +173,6 @@ def getUsers(){
     }
   }
   return map
-}
-
-def getUser(int idUser){
-  def get = getConnection('https://api.ganttpro.com/v1.0/users/'+idUser)
-  if (get.getResponseCode().equals(200)) {
-    return getListData(get)
-  }
-}
-
-def getTask(int idTask){
-  def data
-  def get = getConnection('https://api.ganttpro.com/v1.0/tasks/'+idTask)
-  if (get.getResponseCode().equals(200)) {
-    data = getListData(get)
-
-  }
-
-    return data
 }
 
 def getConnection(String url){
